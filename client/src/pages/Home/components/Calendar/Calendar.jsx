49535238b0818f2f78a52e1b1b9efe40603a6de2
rmeizo-m <vinnikove21@gmail.com>
2020-08-20 17:02:57 +0300

    mini-fix2

--- client/src/pages/Home/components/Calendar/Calendar.jsx
+++ client/src/pages/Home/components/Calendar/Calendar.jsx
@@ -9,9 +9,9 @@ export const CalendarHome = () => {
     <>
       <div className={s.row}>Friday</div>
       <div className={s.date}>
-        <div>Mar</div>
-        <div>13</div>
-        <div>2020</div>
+        <div className={s.monsd}>Aug</div>
+        <div className={s.day}>20</div>
+        <div className={s.yaer}>2020</div>
       </div>
       <Calendar 
          clasName={["react-calendar", "react-calendar__tile "]} 
